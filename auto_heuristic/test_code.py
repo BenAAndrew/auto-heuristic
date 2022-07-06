738d84fe98d793015f15df2efb8121545529129f
BenAAndrew <benandrew89@gmail.com>
2022-07-05 17:49:25 -0700

    setup testing

---
+++ auto_heuristic/test_code.py
@@ -0,0 +1,27 @@
+from auto_heuristic.code import decision_tree_to_js, decision_tree_to_python, decision_tree_to_text
+from auto_heuristic.tree import DecisionNode
+import os
+
+CHILD_TRUE_BRANCH = DecisionNode("petal width", 1.65, "versicolor", "virginica")
+CHILD_FALSE_BRANCH = DecisionNode("petal width", 1.75, "versicolor", "virginica")
+FALSE_BRANCH = DecisionNode("petal length", 4.75, CHILD_TRUE_BRANCH, CHILD_FALSE_BRANCH)
+DECISION_TREE = DecisionNode("petal length", 2.45, "setosa", FALSE_BRANCH)
+FEATURE_NAMES = ["petal width", "petal length"]
+
+
+def test_decision_tree_to_text():
+    with open(os.path.join("test_files", "sample.txt")) as f:
+        text = f.read()
+    assert decision_tree_to_text(DECISION_TREE) + "\n" == text
+
+
+def test_decision_tree_to_python():
+    with open(os.path.join("test_files", "sample.py")) as f:
+        python_code = f.read()
+    assert decision_tree_to_python(DECISION_TREE, FEATURE_NAMES) + "\n" == python_code
+
+
+def test_decision_tree_to_js():
+    with open(os.path.join("test_files", "sample.js")) as f:
+        js_code = f.read()
+    assert decision_tree_to_js(DECISION_TREE, FEATURE_NAMES) + "\n" == js_code
